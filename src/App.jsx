d84f423b64bd250512e12e5c9cf6018d5a282c7d
Tacarrem23 <carremitchell729@gmail.com>
2025-05-20 14:24:45 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export default function App() {
   return <></>;
 }
+
+// App.jsx
+import React from 'react';
+import { useTheme } from './ThemeContext';
+import Navbar from './Navbar';
+import Specimen from './Specimen';
+
+const App = () => {
+  const { theme } = useTheme();
+
+  return (
+    <div className={theme}>
+      <Navbar />
+      <ul>
+        <Specimen name="Example 1" shouldGlow={true} />
+        <Specimen name="Example 2" shouldGlow={false} />
+      </ul>
+    </div>
+  );
+};
+
+export default App;
